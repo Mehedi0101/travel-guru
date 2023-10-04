81b29d500ea33ba0814fb2fcfa178dd877ce54a2
Mehedi0101 <mehedih2909@gmail.com>
2023-10-04 13:20:28 +0600

    responsive navbar added

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#F9A51A'
+      },
+      fontFamily: {
+        'bebas': ['Bebas Neue', 'sans-serif']
+      }
+    }
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 }
